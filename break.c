cb1819d43ae9711215e4af0f0a11e32190f29b8c
Gagan Prajapati <142085091+Gagan3036@users.noreply.github.com>
2024-02-18 22:47:27 +0530

    Update break.c

--- break.c
+++ break.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
 int man() {
+    /*Break statement:- Skips all remaining iterations and come out
+    of the loop */
     int i;
-    for(i = 1; i % 2; i++) {
-        printf("%d",i);
+    for(i = 1; i <=10; i++) {
+        printf("%d\n",i);
+        if (i == 5){
+            break; //After reaching 5 it will break the loop 
+        }
     }
 }
