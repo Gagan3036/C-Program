57e985fb015e3715c53a2c7dce8607479d6457c1
Gagan Prajapati <142085091+Gagan3036@users.noreply.github.com>
2024-02-18 22:48:32 +0530

    Update entry.c

--- entry.c
+++ entry.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int main () {
-    //Entry control loop 
+    /*Entry control loop:- Condition will excute
+    first then statement.*/
     int i=1, sum;
     while(i<=10){  //condition i, 1-->10 will execute first simultaneously 
         printf("%d\n",i); //simultaneously printing of i after chacking i<=10
